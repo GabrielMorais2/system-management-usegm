8482237049576abf678b8a42ee71a6007f493913
Gabriel Moraes da Silva <silvagabriel06@sicredi.com.br>
2024-12-25 19:17:18 -0300

    git init

---
+++ backend/src/main/java/br/com/system/usegm/UsegmApplication.java
@@ -0,0 +1,13 @@
+package br.com.system.usegm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsegmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsegmApplication.class, args);
+	}
+
+}
